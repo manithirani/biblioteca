d26522dd068ea0dfb0501d9ca7b2e390a1140cff
manithirani <manithirani@gmail.com>
2012-07-17 23:28:15 +0530

    Book and tests related to Book

---
+++ src/com/twu28/biblioteca/Book1.java
@@ -0,0 +1,46 @@
+package com.twu28.biblioteca;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: M@N!T
+ * Date: 7/17/12
+ * Time: 10:32 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Book1 {
+
+    boolean availability;
+    int num;
+    String name;
+    public Book1(String n, int no){
+        this.name=n;
+        this.num=no;
+        availability=true;
+    }
+    public String getName() {
+        return this.name;
+    }
+
+    public int getNum() {
+        return this.num;
+    }
+
+    public boolean getAvail() {
+        return this.availability;
+    }
+
+    public void reserve() {
+        if(this.getAvail())
+        {
+            this.setAvail(false);
+            System.out.println("Enjoy the book. Thank You");
+        }
+        else
+            System.out.println("Book is not available");
+
+    }
+
+    private void setAvail(boolean b) {
+        this.availability=b;
+    }
+}
---
+++ test/com/twu28/biblioteca/BookTest.java
@@ -0,0 +1,35 @@
+package com.twu28.biblioteca;
+
+import junit.framework.TestCase;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: M@N!T
+ * Date: 7/17/12
+ * Time: 10:29 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class BookTest extends TestCase {
+
+    public void testbookShouldHaveAName() throws Exception {
+        Book1 book=new Book1("XYZ",1);
+        assertEquals("XYZ",book.getName());
+
+    }
+    public void testbookShouldHaveANumber() throws Exception {
+        Book1 book=new Book1("XYZ", 1);
+        assertEquals(1 ,book.getNum());
+
+    }
+    public void testbookShouldBeAvailableWhenCreated() throws Exception {
+        Book1 book=new Book1("XYZ", 1);
+        assertTrue(book.getAvail());
+
+    }
+    public void testbookShouldNotBeAvailableWhenReserved() throws Exception {
+        Book1 book=new Book1("XYZ", 1);
+        book.reserve();
+        assertFalse(book.getAvail());
+
+    }
+}
